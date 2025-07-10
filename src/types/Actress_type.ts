56a5f59a86f737412d7eef695d9ce0c2ececd2f1
mattiacoli <mattia.coli@aol.com>
2025-07-10 11:15:42 +0200

    fix nationality

--- src/types/Actress_type.ts
+++ src/types/Actress_type.ts
@@ -1,7 +1,20 @@
 import { Person } from "./Person_type"
 
+
+type ActressNationality =
+  | "American"
+  | "British"
+  | "Australian"
+  | " Israeli - American"
+  | "South African"
+  | "French"
+  | "Indian"
+  | "Spanish"
+  | " South Korean"
+  | "Chinese"
+
 export type Actress = Person & {
   most_famous_movies: [string, string, string],
   awards: string,
-  nationality: string
+  nationality: ActressNationality
 }
