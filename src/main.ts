eb1b90b10f43fd5c7d87cd12f7d1bd2214b5995f
mattiacoli <mattia.coli@aol.com>
2025-07-14 10:59:42 +0200

    fix: streamline isActress function checks and improve error handling in getActress

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,18 @@ import { Actress } from "./types/Actress_type";
 function isActress(actress: unknown): actress is Actress {
   if (
     actress &&
-    typeof actress === "object" &&
-    "id" in actress &&
-    typeof actress.id === "number" &&
-    "name" in actress &&
-    typeof actress.name === "string" &&
-    "birth_year" in actress &&
-    typeof actress.birth_year === "number" &&
-    "biography" in actress &&
-    typeof actress.biography === "string" &&
-    "image" in actress &&
-    typeof actress.image === "string" &&
+    typeof actress === "object" && actress != null &&
+    "id" in actress && typeof actress.id === "number" &&
+    "name" in actress && typeof actress.name === "string" &&
+    "birth_year" in actress && typeof actress.birth_year === "number" &&
+    "biography" in actress && typeof actress.biography === "string" &&
+    "image" in actress && typeof actress.image === "string" &&
     "most_famous_movies" in actress &&
-    Array.isArray(actress.most_famous_movies) &&
-    "awards" in actress &&
-    typeof actress.awards === "string" &&
-    "nationality" in actress &&
-    typeof actress.id === "string"
+    actress.most_famous_movies instanceof Array &&
+    actress.most_famous_movies.length === 3 &&
+    actress.most_famous_movies.every(m => typeof m === 'string') &&
+    "awards" in actress && typeof actress.awards === "string" &&
+    "nationality" in actress && typeof actress.nationality === "string"
   ) {
     return true
   }
@@ -40,7 +35,11 @@ async function getActress(id: number): Promise<Actress | null> {
     }
     return actress
   } catch (error) {
-    console.error(error)
+    if (error instanceof Error) {
+      console.error('Errore durante il recupero di dati', error)
+    } else {
+      console.error('Unknown Error', error)
+    }
     return null
   }
 }
@@ -71,3 +70,7 @@ async function getActresses(ids: number[]): Promise<Actress[] | null> {
     return null
   }
 }
+
+
+const attrici = getAllActresses()
+console.log(attrici)
